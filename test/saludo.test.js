8d1b9a6483ccec61ccf5b2857c2926ac727e1afe
Gabriel Barraza <141535184+wolfgabo@users.noreply.github.com>
2023-10-07 12:28:39 -0600

    Prueba Federico

--- test/saludo.test.js
+++ test/saludo.test.js
@@ -1,15 +1,22 @@
 const saludo = require("../src/saludo")
 
 test("Gabriel", function () {
-   
+
     expect(saludo("Gabriel")).toEqual("Hola Gabriel!");
 });
 
 const saludo = require("../src/saludo")
 
 test("Juan", function () {
-   
+
     expect(saludo("Juan")).toEqual("Hola Juan!");
 });
+
+const saludo = require("../src/saludo")
+
+test("Federico", function () {
+    expect(saludo("Federico")).toEqual("Hola Federico!");
+});
+
 
 
