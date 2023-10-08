28e3c31ef3af401fe73cfd45f23dd1f37b3edd36
Benjamin Cisneros Barraza <cisnerosbarraza@gmail>
2023-10-08 05:09:39 -0600

    Correcciones a examen

--- src/saludo.js
+++ src/saludo.js
@@ -1,18 +1,17 @@
-function saludo (nombre){
-    
-    // Condicional para Maximiliano, retornando las 3 primeras letras
-    if(nombre.length >= 10){
-        return "Hola " + nombre.slice(0, 3) + "!";
-    }
-    if(nombre === "Federico"){
-        return "Hola Kiko!";
-    }else{
-        return "Hola " + nombre[0].toUpperCase() + nombre.slice(1).toLowerCase() + "!";
-    }
-    
+function saludo(nombre) {
+  if (!nombre || !nombre.trim()) {
+    return "Hola Fulano!";
+  }
+  // Condicional para Maximiliano, retornando las 3 primeras letras
+  if (nombre.length >= 10) {
+    return "Hola " + nombre.slice(0, 3) + "!";
+  }
+  if (nombre === "Federico") {
+    return "Hola Kiko!";
+  }
+  return (
+    "Hola " + nombre[0].toUpperCase() + nombre.slice(1).toLowerCase() + "!"
+  );
 }
 
-module.exports = saludo;
-
-
-
+module.exports = saludo;
